1819e8fabb8053c92439b50a18744dc514e51d3c
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-04-07 04:55:34 +0000

    should be passing the size of the destination, not source to strncpy.
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@267 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/lib/ibBdChrConfig.c
+++ linux-gpib/lib/ibBdChrConfig.c
@@ -27,7 +27,7 @@ int ibBdChrConfig(int ud)
 		ibPutErrlog(ud,"ibBdChrConfig");
 	}else 
 	{
-		strncpy(boardtype.name, board->board_type, sizeof(board->board_type));
+		strncpy(boardtype.name, board->board_type, sizeof(boardtype.name));
 		ioctl(board->fileno, CFCBOARDTYPE, &boardtype); 
 		if(board->base != 0) ibBoardFunc(conf->board, CFCBASE, board->base);
 		if(board->irq  != 0) ibBoardFunc(conf->board, CFCIRQ , board->irq);
