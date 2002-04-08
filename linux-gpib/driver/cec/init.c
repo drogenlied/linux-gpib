1ccb5b075dba011399dc0445d0c47e78eaf04fc6
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-04-08 18:21:59 +0000

    trying to debug cec_pci interrupts
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@268 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/driver/cec/init.c
+++ linux-gpib/driver/cec/init.c
@@ -236,7 +236,7 @@ int cec_pci_attach(gpib_board_t *board)
 	cec_priv->irq = cec_priv->pci_device->irq;
 
 	// enable interrupts on plx chip
-	outl(LINTR1_EN_BIT | PCI_INTR_EN_BIT,
+	outl(LINTR1_EN_BIT | LINTR2_EN_BIT | PCI_INTR_EN_BIT,
 		cec_priv->plx_iobase + PLX_INTCSR_REG);
 
 	cec_init(cec_priv);
--- linux-gpib/driver/cec/interrupt.c
+++ linux-gpib/driver/cec/interrupt.c
@@ -28,17 +28,40 @@ void cec_interrupt(int irq, void *arg, struct pt_regs *registerp)
 {
 	gpib_board_t *board = arg;
 	cec_private_t *priv = board->private_data;
+	unsigned int plx_csr_bits;
 
-printk("plx intcsr 0x%x\n", inl(priv->plx_iobase + PLX_INTCSR_REG));
+plx_csr_bits = inl(priv->plx_iobase + PLX_INTCSR_REG); 
+printk("plx intcsr 0x%x\n", plx_csr_bits);
 
 	nec7210_interrupt(board, &priv->nec7210_priv);
 
-// crash safety : if interrupt didn't clear, disable it
-if(inl(priv->plx_iobase + PLX_INTCSR_REG) & LINTR1_STATUS_BIT) 
+// crash safety : if interrupt didn't clear, try flipping polarity then disabling
+plx_csr_bits = inl(priv->plx_iobase + PLX_INTCSR_REG); 
+if(plx_csr_bits & LINTR1_STATUS_BIT)
 {
-	printk("cec-gpib: ack, interrupt didn't clear.  Disabling interrupt.\n");
-	outl(0, priv->plx_iobase + PLX_INTCSR_REG);
+	if((plx_csr_bits & LINTR1_POLARITY_BIT)) 
+	{
+		printk("cec-gpib: ack, local interrupt 1 didn't clear.  Disabling.\n");
+		plx_csr_bits &= ~LINTR1_EN_BIT;
+	} else
+	{
+		printk("cec-gpib: ack, local interrupt 1 didn't clear.  Flipping polarity.\n");
+		plx_csr_bits |= LINTR1_POLARITY_BIT;
+	}	
 }
-
+if(plx_csr_bits & LINTR2_STATUS_BIT) 
+{
+	if((plx_csr_bits & LINTR2_POLARITY_BIT)) 
+	{
+		printk("cec-gpib: ack, local interrupt 2 didn't clear.  Disabling.\n");
+		plx_csr_bits &= ~LINTR2_EN_BIT;
+	} else
+	{
+		printk("cec-gpib: ack, local interrupt 2 didn't clear.  Flipping polarity.\n");
+		plx_csr_bits |= LINTR2_POLARITY_BIT;
+	}	
+}
+if(plx_csr_bits & (LINTR1_STATUS_BIT | LINTR2_STATUS_BIT)) 
+	outl(plx_csr_bits, priv->plx_iobase + PLX_INTCSR_REG);
 }
 
