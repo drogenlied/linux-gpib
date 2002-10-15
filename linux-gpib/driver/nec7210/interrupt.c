4c57011d89c87d36a3329b0896232432e029b9e0
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-10-15 17:37:13 +0000

    be a little smarter about not printing interrupt debug spam on shared interrupt
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@409 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/driver/nec7210/interrupt.c
+++ linux-gpib/driver/nec7210/interrupt.c
@@ -164,7 +164,7 @@ void nec7210_interrupt(gpib_board_t *board, nec7210_private_t *priv)
 
 	spin_unlock(&board->spinlock);
 
-	if( status1 | status2 )
+	if( ( status1 & priv->imr1_bits ) || ( status2 & priv->imr2_bits ) )
 	{
 		GPIB_DPRINTK( "isr1 0x%x, imr1 0x%x, isr2 0x%x, imr2 0x%x, status 0x%x\n",
 			status1, priv->imr1_bits, status2, priv->imr2_bits, board->status);
