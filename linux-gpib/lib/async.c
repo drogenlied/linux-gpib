64d49bf9a64d0eed0c6b950cd262fd1a893a9392
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-10-24 01:14:28 +0000

    new file for supporting asynchronous io
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@425 b61deed1-901b-0410-a6dd-e94ecbdaea89

---
+++ linux-gpib/lib/async.c
@@ -0,0 +1,30 @@
+/***************************************************************************
+                          lib/async.c
+                             -------------------
+
+    copyright            : (C) 2002 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "ib_internal.h"
+#include <ibP.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
+
+void init_async_op( struct async_operation *async )
+{
+	pthread_mutex_init( &async->lock, NULL );
+	async->buffer = NULL;
+	async->length = 0;
+	async->error = 0;
+	async->in_progress = 0;
+}
