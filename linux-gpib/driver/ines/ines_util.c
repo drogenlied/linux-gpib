946d51cdbfc1ee8a616f6cde5b590e12f4e41edf
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-11-21 18:08:00 +0000

    added definition of ines_line_status()
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@451 b61deed1-901b-0410-a6dd-e94ecbdaea89

---
+++ linux-gpib/driver/ines/ines_util.c
@@ -0,0 +1,57 @@
+/***************************************************************************
+                          nec7210/ines_util.c  -  description
+                             -------------------
+    copyright            : (C) 2002 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "ines.h"
+
+#include <linux/pci.h>
+#include <asm/io.h>
+#include <linux/module.h>
+
+int ines_line_status( const gpib_board_t *board )
+{
+	int status = ValidALL;
+	int bcm_bits;
+	ines_private_t *ines_priv;
+	nec7210_private_t *nec_priv;
+
+	ines_priv = board->private_data;
+	nec_priv = &ines_priv->nec7210_priv;
+
+	bcm_bits = inb( ines_priv->nec7210_priv.iobase + BUS_CONTROL_MONITOR );
+
+	if( bcm_bits & BCM_REN_BIT )
+		status |= BusREN;
+	if( bcm_bits & BCM_IFC_BIT )
+		status |= BusIFC;
+	if( bcm_bits & BCM_SRQ_BIT )
+		status |= BusSRQ;
+	if( bcm_bits & BCM_EOI_BIT )
+		status |= BusEOI;
+	if( bcm_bits & BCM_NRFD_BIT )
+		status |= BusNRFD;
+	if( bcm_bits & BCM_NDAC_BIT )
+		status |= BusNDAC;
+	if( bcm_bits & BCM_DAV_BIT )
+		status |= BusDAV;
+	if( bcm_bits & BCM_ATN_BIT )
+		status |= BusATN;
+
+	return status;
+}
+
+
+
+
