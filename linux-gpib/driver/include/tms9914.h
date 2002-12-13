995dacc0c6f92c1676a272757a48694ac292e6df
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2002-12-13 02:29:23 +0000

    reduce use of preprocessor
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@477 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/driver/include/tms9914.h
+++ linux-gpib/driver/include/tms9914.h
@@ -133,56 +133,74 @@ enum
 //bit definitions common to tms9914 compatible registers
 
 /* ISR0   - Register bits */
-#define     HR_MAC     (unsigned char) (1<<0)   /* My Address Change           */
-#define     HR_RLC     (unsigned char) (1<<1)   /* Remote/Local change         */
-#define     HR_SPAS    (unsigned char) (1<<2)   /* Serial Poll active State    */
-#define     HR_END     (unsigned char) (1<<3)   /* END (EOI or EOS)            */
-#define     HR_BO      (unsigned char) (1<<4)   /* Byte Out                    */
-#define     HR_BI      (unsigned char) (1<<5)   /* Byte In                     */
+enum isr0_bits
+{
+	HR_MAC = ( 1 << 0 ),   /* My Address Change           */
+	HR_RLC = ( 1 << 1 ),   /* Remote/Local change         */
+	HR_SPAS = ( 1 << 2 ),   /* Serial Poll active State    */
+	HR_END = ( 1 << 3 ),   /* END (EOI or EOS)            */
+	HR_BO = ( 1 << 4 ),   /* Byte Out                    */
+	HR_BI = ( 1 << 5 ),   /* Byte In                     */
+};
 
 /* IMR0   - Register bits */
-#define     HR_MACIE   (unsigned char) (1<<0)   /*        */
-#define     HR_RLCIE   (unsigned char) (1<<1)   /*        */
-#define     HR_SPASIE  (unsigned char) (1<<2)   /*        */
-#define     HR_ENDIE   (unsigned char) (1<<3)   /*        */
-#define     HR_BOIE    (unsigned char) (1<<4)   /*        */
-#define     HR_BIIE    (unsigned char) (1<<5)   /*        */
+enum imr0_bits
+{
+	HR_MACIE = ( 1 << 0 ),   /*        */
+	HR_RLCIE = ( 1 << 1 ),   /*        */
+	HR_SPASIE = ( 1 << 2 ),   /*        */
+	HR_ENDIE = ( 1 << 3 ),   /*        */
+	HR_BOIE = ( 1 << 4 ),   /*        */
+	HR_BIIE = ( 1 << 5 ),   /*        */
+};
 
 /* ISR1   - Register bits */
-#define     HR_IFC     (unsigned char) (1<<0)   /* IFC asserted                */
-#define     HR_SRQ     (unsigned char) (1<<1)   /* SRQ asserted                */
-#define     HR_MA      (unsigned char) (1<<2)   /* My Adress                   */
-#define     HR_DCAS    (unsigned char) (1<<3)   /* Device Clear active State   */
-#define     HR_APT     (unsigned char) (1<<4)   /* Adress pass Through         */
-#define     HR_UNC     (unsigned char) (1<<5)   /* Unrecognized Command        */
-#define     HR_ERR     (unsigned char) (1<<6)   /* Data Transmission Error     */
-#define     HR_GET     (unsigned char) (1<<7)   /* Group execute Trigger       */
+enum isr1_bits
+{
+	HR_IFC = ( 1 << 0 ),   /* IFC asserted                */
+	HR_SRQ = ( 1 << 1 ),   /* SRQ asserted                */
+	HR_MA = ( 1 << 2 ),   /* My Adress                   */
+	HR_DCAS = ( 1 << 3 ),   /* Device Clear active State   */
+	HR_APT = ( 1 << 4 ),   /* Adress pass Through         */
+	HR_UNC = ( 1 << 5 ),   /* Unrecognized Command        */
+	HR_ERR = ( 1 << 6 ),   /* Data Transmission Error     */
+	HR_GET = ( 1 << 7 ),   /* Group execute Trigger       */
+};
 
 /* IMR1   - Register bits */
-#define     HR_IFCIE   (unsigned char) (1<<0)   /*        */
-#define     HR_SRQIE   (unsigned char) (1<<1)   /*        */
-#define     HR_MAIE    (unsigned char) (1<<2)   /*        */
-#define     HR_DCASIE  (unsigned char) (1<<3)   /*        */
-#define     HR_APTIE   (unsigned char) (1<<4)   /*        */
-#define     HR_UNCIE   (unsigned char) (1<<5)   /*        */
-#define     HR_ERRIE   (unsigned char) (1<<6)   /*        */
-#define     HR_GETIE   (unsigned char) (1<<7)   /*        */
+enum imr1_bits
+{
+	HR_IFCIE = ( 1 << 0 ),   /*        */
+	HR_SRQIE = ( 1 << 1 ),   /*        */
+	HR_MAIE = ( 1 << 2 ),   /*        */
+	HR_DCASIE = ( 1 << 3 ),   /*        */
+	HR_APTIE = ( 1 << 4 ),   /*        */
+	HR_UNCIE = ( 1 << 5 ),   /*        */
+	HR_ERRIE = ( 1 << 6 ),   /*        */
+	HR_GETIE = ( 1 << 7 ),   /*        */
+};
 
 /* ADSR   - Register bits */
-#define     HR_ULPA    (unsigned char) (1<<0)   /* Store last address LSB      */
-#define     HR_TA      (unsigned char) (1<<1)   /* Talker Adressed             */
-#define     HR_LA      (unsigned char) (1<<2)   /* Listener adressed           */
-#define     HR_TPAS    (unsigned char) (1<<3)   /* talker primary adress state */
-#define     HR_LPAS    (unsigned char) (1<<4)   /* listener    "               */
-#define     HR_ATN     (unsigned char) (1<<5)   /* ATN active                  */
-#define     HR_LLO     (unsigned char) (1<<6)   /* LLO active                  */
-#define     HR_REM     (unsigned char) (1<<7)   /* REM active                  */
+enum adsr_bits
+{
+	HR_ULPA = ( 1 << 0 ),   /* Store last address LSB      */
+	HR_TA = ( 1 << 1 ),   /* Talker Adressed             */
+	HR_LA = ( 1 << 2 ),   /* Listener adressed           */
+	HR_TPAS = ( 1 << 3 ),   /* talker primary adress state */
+	HR_LPAS = ( 1 << 4 ),   /* listener    "               */
+	HR_ATN = ( 1 << 5 ),   /* ATN active                  */
+	HR_LLO = ( 1 << 6 ),   /* LLO active                  */
+	HR_REM = ( 1 << 7 ),   /* REM active                  */
+};
 
 /* ADR   - Register bits */
-#define	ADDRESS_MASK		0x1f			/* mask to specify lower 5 bits for ADR */
-#define     HR_DAT     (unsigned char) (1<<5)   /*        */
-#define     HR_DAL     (unsigned char) (1<<6)   /*        */
-#define     HR_EDPA    (unsigned char) (1<<7)   /*        */
+enum adr_bits
+{
+	ADDRESS_MASK = 0x1f,	/* mask to specify lower 5 bits for ADR */
+	HR_DAT = ( 1 << 5 ),   /*        */
+	HR_DAL = ( 1 << 6 ),   /*        */
+	HR_EDPA = ( 1 << 7 ),   /*        */
+};
 
 enum bus_status_bits
 {
@@ -200,33 +218,35 @@ enum bus_status_bits
 /* TMS 9914 Auxiliary Commands                             */
 /*---------------------------------------------------------*/
 
-#define AUX_CR	0x0     /* d Chip reset                   */
-#define AUX_DHDF 0x1	// release dac holdoff (nonvalid)
-#define AUX_VAL	(AUX_DHDF | AUX_CS)	// release dac holdoff, valid
-#define AUX_RHDF	0x2     /* X Release RFD holdoff          */
-#define AUX_HLDA	0x3     /* d holdoff on all data          */
-#define AUX_HLDE	0x4     /* d holdoff on EOI only          */
-#define AUX_NBAF	0x5     /* X Set ne byte availiable false */
-#define AUX_FGET	0x6     /* d force GET                    */
-#define AUX_RTL	0x7     /* d return to local              */
-#define AUX_SEOI	0x8     /* X send EOI with next byte      */
-#define AUX_LON	0x9     /* d Listen only                  */
-#define AUX_TON	0xa    /* d Talk only                    */
-#define AUX_GTS	0xb    /* X goto standby                 */
-#define AUX_TCA	0xc    /* X take control asynchronously  */
-#define AUX_TCS	0xd    /* X take    "     synchronously  */
-#define AUX_RPP	0xe    /* d Request parallel poll        */
-#define AUX_SIC	0xf    /* d send interface clear         */
-#define AUX_SRE	0x10    /* d send remote enable           */
-#define AUX_RQC	0x11    /* X request control              */
-#define AUX_RLC	0x12    /* X release control              */
-#define AUX_DAI	0x13    /* d disable all interrupts       */
-#define AUX_PTS	0x14    /* X pass through next secondary  */
-#define AUX_STDL	0x15    /* d short T1 delay                 */
-#define AUX_SHDW	0x16    /* d shadow handshake             */
-#define AUX_VSTDL	0x17    /* d very short T1 delay              */
-#define AUX_RSV2	0x18	// d request service bit 2
-
-#define AUX_CS	0x80 /* set bit instead of clearing it     */
+enum aux_cmd_bits
+{
+	AUX_CS = 0x80,	/* set bit instead of clearing it, used with commands marked 'd' above     */
+	AUX_CR = 0x0,	/* d Chip reset                   */
+	AUX_DHDF = 0x1,	// release dac holdoff (nonvalid)
+	AUX_VAL = ( AUX_DHDF | AUX_CS ),	// release dac holdoff, valid
+	AUX_RHDF = 0x2,	/* X Release RFD holdoff          */
+	AUX_HLDA = 0x3,	/* d holdoff on all data          */
+	AUX_HLDE = 0x4,	/* d holdoff on EOI only          */
+	AUX_NBAF = 0x5,	/* X Set ne byte availiable false */
+	AUX_FGET = 0x6,	/* d force GET                    */
+	AUX_RTL = 0x7,	/* d return to local              */
+	AUX_SEOI = 0x8,	/* X send EOI with next byte      */
+	AUX_LON = 0x9,	/* d Listen only                  */
+	AUX_TON = 0xa,	/* d Talk only                    */
+	AUX_GTS = 0xb,	/* X goto standby                 */
+	AUX_TCA = 0xc,	/* X take control asynchronously  */
+	AUX_TCS = 0xd,	/* X take    "     synchronously  */
+	AUX_RPP = 0xe,	/* d Request parallel poll        */
+	AUX_SIC = 0xf,	/* d send interface clear         */
+	AUX_SRE = 0x10,	/* d send remote enable           */
+	AUX_RQC = 0x11,	/* X request control              */
+	AUX_RLC = 0x12,	/* X release control              */
+	AUX_DAI = 0x13,	/* d disable all interrupts       */
+	AUX_PTS = 0x14,	/* X pass through next secondary  */
+	AUX_STDL = 0x15,	/* d short T1 delay                 */
+	AUX_SHDW = 0x16,	/* d shadow handshake             */
+	AUX_VSTDL = 0x17,	/* d very short T1 delay              */
+	AUX_RSV2 = 0x18,	// d request service bit 2
+};
 
 #endif	//_TMS9914_H
