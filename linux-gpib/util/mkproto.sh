a39cfcc4b80e0c9d638d6113a7e86a5f3b2b3f33
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2003-07-14 23:06:14 +0000

    fixing 'make dist' and cleaning up some cruft
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@798 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/util/mkproto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd $1
-echo "/* Automatically created by mkproto.sh. DONT EDIT */" 
-echo "/***** Public Functions ******/" 
-grep -h IBLCL *.c|/lib/cpp -P -DIBLCL=extern -DVOID=void| sed 's/)/);/' 
