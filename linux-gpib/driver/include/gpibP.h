ae4fef308ce5b49d717b3ef4793b3b913c7fcee2
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2003-12-10 01:42:59 +0000

    added gpl boilerplate
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@945 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/driver/include/gpibP.h
+++ linux-gpib/driver/include/gpibP.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          gpibP.h
+                             -------------------
+
+    copyright            : (C) 2002,2003 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef _GPIB_P_H
 #define _GPIB_P_H
