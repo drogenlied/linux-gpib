ba3a7aef7c189cbf8e2d72aa0a550db45cec54f4
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2004-12-24 21:16:02 +0000

    reversed some of Robert Jordan's packaging changes that cause problems with "normal" compilation/installation
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@1162 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/language/python/setup.py
+++ linux-gpib/language/python/setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup,Extension
 setup(name="gpib",
 	version="1.0",
 	description="Linux GPIB Python Bindings",
-	py_modules = ['language/python/Gpib'],
+	py_modules = ['Gpib'],
 	ext_modules=[
 		Extension("gpib",
-		["language/python/gpibinter.c"],
-		include_dirs=["include"],
-		library_dirs = ['lib/.libs'],
+		["gpibinter.c"],
+		include_dirs=["../../include"],
+		library_dirs = ['../../lib/.libs'],
 		libraries=["gpib", "pthread"]
 	)]
 )
