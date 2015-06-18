629048ca6f1db7c4fdb56e901325a708a0e6e35b
dpenkler <dpenkler@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2015-06-18 18:14:31 +0000

    Add lib/ibVers.c file to svn
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@1636 b61deed1-901b-0410-a6dd-e94ecbdaea89

---
+++ linux-gpib/lib/ibVers.c
@@ -0,0 +1,33 @@
+/***************************************************************************
+                          lib/ibVers.c
+                          ------------
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#define xstr(s) str(s)
+#define str(s) #s
+static char *my_version = xstr(GPIB_VERSION);
+void ibvers( char **vers )
+{
+  *vers = my_version;
+}
+
+
+
+
+
+
+
+
+
+
+
