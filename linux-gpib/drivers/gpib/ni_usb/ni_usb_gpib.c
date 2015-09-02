4b5b683e8c14dfb89755525253c087f97d64b2da
dpenkler <dpenkler@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2015-09-02 12:51:23 +0000

    Add patch for Keithley KUSB-488 (non-A) support from Dale Smith
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@1640 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib/drivers/gpib/ni_usb/ni_usb_gpib.c
+++ linux-gpib/drivers/gpib/ni_usb/ni_usb_gpib.c
@@ -2041,14 +2041,18 @@ int ni_usb_attach(gpib_board_t *board, gpib_board_config_t config)
 	}
 	product_id = USBID_TO_CPU(interface_to_usbdev(ni_priv->bus_interface)->descriptor.idProduct);
 	printk("\tproduct id=0x%x\n", product_id);
-	if(product_id == USB_DEVICE_ID_NI_USB_B)
+	switch (product_id)
 	{
+	case USB_DEVICE_ID_NI_USB_B:
 		ni_priv->bulk_out_endpoint = NIUSB_B_BULK_OUT_ENDPOINT;
 		ni_priv->bulk_in_endpoint = NIUSB_B_BULK_IN_ENDPOINT;
 		ni_priv->interrupt_in_endpoint = NIUSB_B_INTERRUPT_IN_ENDPOINT;
 		ni_usb_b_read_serial_number(ni_priv);
-	}else if(product_id == USB_DEVICE_ID_NI_USB_HS || product_id == USB_DEVICE_ID_MC_USB_488 || product_id == USB_DEVICE_ID_KUSB_488A)
-	{
+		break;
+	case USB_DEVICE_ID_NI_USB_HS:
+	case USB_DEVICE_ID_MC_USB_488:
+	case USB_DEVICE_ID_KUSB_488:
+	case USB_DEVICE_ID_KUSB_488A:
 		ni_priv->bulk_out_endpoint = NIUSB_HS_BULK_OUT_ENDPOINT;
 		ni_priv->bulk_in_endpoint = NIUSB_HS_BULK_IN_ENDPOINT;
 		ni_priv->interrupt_in_endpoint = NIUSB_HS_INTERRUPT_IN_ENDPOINT;
@@ -2058,8 +2062,8 @@ int ni_usb_attach(gpib_board_t *board, gpib_board_config_t config)
 			mutex_unlock(&ni_usb_hotplug_lock);
 			return retval;
 		}
-	}else
-	{
+		break;
+	default:
 		mutex_unlock(&ni_usb_hotplug_lock);
 		printk("\tDriver bug: unknown endpoints for usb device id\n");
 		return -EINVAL;
@@ -2182,6 +2186,7 @@ static struct usb_device_id ni_usb_driver_device_table [] =
 {
 	{USB_DEVICE(USB_VENDOR_ID_NI, USB_DEVICE_ID_NI_USB_B)},
 	{USB_DEVICE(USB_VENDOR_ID_NI, USB_DEVICE_ID_NI_USB_HS)},
+	{USB_DEVICE(USB_VENDOR_ID_NI, USB_DEVICE_ID_KUSB_488)},
 	{USB_DEVICE(USB_VENDOR_ID_NI, USB_DEVICE_ID_KUSB_488A)},
 	{USB_DEVICE(USB_VENDOR_ID_NI, USB_DEVICE_ID_MC_USB_488)},
 	{} /* Terminating entry */
