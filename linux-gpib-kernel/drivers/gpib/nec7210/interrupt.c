310de395d0353d949a6fa2e92053df6552ac61d3
fmhess <fmhess@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2019-08-23 17:29:09 +0000

    Mask command byte read from command passthrough register on nec7210 (makes no practical difference at this point).
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@1847 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib-kernel/drivers/gpib/nec7210/interrupt.c
+++ linux-gpib-kernel/drivers/gpib/nec7210/interrupt.c
@@ -151,7 +151,7 @@ irqreturn_t nec7210_interrupt_have_status( gpib_board_t *board,
 	{
 		unsigned int command;
 
-		command = read_byte(priv, CPTR);
+		command = read_byte(priv, CPTR) & gpib_command_mask;
 		write_byte(priv, AUX_NVAL, AUXMR);
 		printk( "gpib: command pass through 0x%x\n", command );
 	}
