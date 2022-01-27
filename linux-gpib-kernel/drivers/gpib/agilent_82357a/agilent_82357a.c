bfd8749fa47b3567bfa49d804c7c2d7841291b30
dpenkler <dpenkler@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2022-01-27 10:05:20 +0000

    Avoid fallthrough warning
    git-svn-id: https://svn.code.sf.net/p/linux-gpib/code/trunk@1997 b61deed1-901b-0410-a6dd-e94ecbdaea89

--- linux-gpib-kernel/drivers/gpib/agilent_82357a/agilent_82357a.c
+++ linux-gpib-kernel/drivers/gpib/agilent_82357a/agilent_82357a.c
@@ -453,7 +453,8 @@ static int agilent_82357a_abort(agilent_82357a_private_t *a_priv, int flush)
 			retval = 0;
 			break;
 		}
-		//fall-through
+		fallthrough;
+		// fall through
 	case UGP_ERR_FLUSHING_ALREADY:
 	default:
 		printk("%s: abort returned error code=0x%x\n", __FUNCTION__, status_data[1]);
